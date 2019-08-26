398cc9bf69fa1ee658ffd66c28acc78ea69886d8
adriennem76 <adrienne.c.miranda@gmail.com>
2019-08-26 17:46:12 -0400

    simple state

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Cell from "./Cell";
 
 export default class Matrix extends Component {
   
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <Cell value={val}/>) // replace me and render a cell component instead!
+    
   )
   
   genMatrix = () => (
@@ -17,5 +19,8 @@ export default class Matrix extends Component {
       </div>
     )
   }
+
   
 }
+
+Matrix.defaultProps= {values: Array(10).fill(Array(10).fill("#F00"))}
